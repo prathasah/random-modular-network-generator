badcb1c882b2a5220df8efc5f020133e189eed73
prathasah <ps875@georgetown.edu>
2014-02-19 19:37:49 -0500

    Debugged scalefree degree distribution generator

--- sequence_generator.py
+++ sequence_generator.py
@@ -12,41 +12,45 @@ __email__ = "ps875@georgetown.edu"
 
 import numpy as np
 import math
+import random as rnd
 
 ###################################################################################################
 
-def regular_sequence (N, mean):
+def regular_sequence (N, mean, seqtype):
         seq=[mean]*(N)
         return seq
         
 ##############################################################################################   
-def poisson_sequence (N, mean):
+def poisson_sequence (N, mean, seqtype):
 	seq= list(np.random.poisson(lam=mean, size=(N)))
         return seq
 
 ##############################################################################################   
 
-def scalefree_sequence (N, mean):
-	alpha=N/10
+def scalefree_sequence (N, mean, seqtype):
+	
+	if seqtype =="modulesize": alpha=10
+	else: alpha=1/(1.0*mean)*1000
         condition=False
         tol= 5 # set initial tolerance high to enter the loop
         while tol> 0.05:
             seq1= list((np.random. power(alpha, size=(N))))
             seq1=[abs(1-x) for x in seq1]
-            seq=[int(num*(N-1)) for num in seq1]
-            tol = abs(mean-(sum(seq)/(1.0*len(seq)))) 
-                              
+            if seqtype == "modulesize": seq=[int(num*(N*mean-1)) for num in seq1]
+            else:  seq=[int(num*(N-1)) for num in seq1]
+            tol = abs(mean-np.mean(seq))
+                      
             # check if the average of the total-degree list is close to the network mean degree (d). Tolerance=0.5 deviation from d.
             if tol>0.05:
-                if mean-(sum(seq)/(1.0*len(seq)))>0.05:alpha-=0.01
-                else:alpha+=0.01
-       
+                if mean-np.mean(seq)>0.05:alpha-=0.05
+                else:alpha+=0.05
+           
         return seq
 
 ##############################################################################################    
    
 # generate geometric degree sequence
-def geometric_sequence(N, mean):
+def geometric_sequence(N, mean, seqtype):
     max_trial=1000
     avg = mean 
     calc_mean=0
